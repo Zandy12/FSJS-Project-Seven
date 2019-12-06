908454febdf482a3683de78be2da5bd998226903
Zane Chandy <zanechandy@gmail.com>
2019-12-06 16:23:30 -0500

    First commit.

---
+++ src/components/config.js
@@ -0,0 +1,5 @@
+// API key generated from Flickr for access upon signing-up.
+const apiKey = 'd18b36cf6dcfa3bbf8b42b9ddcdae2cc'; 
+//const secret = 'ac3de0390df24098'; // NOTE: May not be needed
+
+export default apiKey;
